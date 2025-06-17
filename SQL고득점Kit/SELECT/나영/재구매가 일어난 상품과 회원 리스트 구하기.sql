c96c34f2030f922ed3c4208484c17e6823fe66f4
na00ee <chuhi1@naver.com>
2025-06-17 23:19:51 +0900

    Solved: PG_재구매가 일어난 상품과 회원 리스트 구하기 ver.HAVING 김나영

---
+++ SQL고득점Kit/SELECT/나영/재구매가 일어난 상품과 회원 리스트 구하기.sql
@@ -0,0 +1,27 @@
+# 방법 1
+SELECT
+    USER_ID, PRODUCT_ID
+FROM
+    ONLINE_SALE 
+GROUP BY 
+    1, 
+    2
+HAVING
+    COUNT(*) > 1
+ORDER BY
+    1,
+    2 DESC
+
+# 방법 2
+SELECT
+    USER_ID, PRODUCT_ID
+FROM
+    ONLINE_SALE 
+GROUP BY 
+    USER_ID, 
+    PRODUCT_ID
+HAVING
+    COUNT(*) > 1
+ORDER BY
+    USER_ID,
+    PRODUCT_ID DESC
